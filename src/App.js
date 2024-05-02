b010324033cb284a32163637b0417da53ba3cb10
dheena5248D <dheenadhayalan5248d@gmail.com>
2024-05-02 13:50:43 +0530

    v0.2 header position bug fixed, top container completed and added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header/header.js'
+import TopContainer from './components/TopContainer/TopContainer.js';
+
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-    
+      <Header />
+      <TopContainer />
     </div>
   );
 }
